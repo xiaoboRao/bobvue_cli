7c4d4db61cc77fa67dad18f873f66737c476e7b9
Bob <731653765@qq.com>
2022-02-03 19:53:11 +0800

    修复获取控制台-d后面参数的缺陷

---
+++ src/components/goods.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="goods">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'goods',
+  props: {
+    msg: String
+  },
+  components: {
+  },
+  mixins: [],
+  data: function() {
+    return {
+      message: "goods"
+    }
+  },
+  created: function() {
+
+  },
+  mounted: function() {
+
+  },
+  computed: {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+  .goods {
+    
+  }
+</style>
+
+
